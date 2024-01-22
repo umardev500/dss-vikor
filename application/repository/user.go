eb479fcaf60ceac4f065da5950dd3de0317151e3
Umar <umar.dev500@gmail.com>
2024-01-22 20:22:28 +0700

    add delete method to users repository

--- application/repository/user.go
+++ application/repository/user.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"github.com/umardev500/spk/config"
+	"github.com/umardev500/spk/constants"
 	"github.com/umardev500/spk/domain"
 	"github.com/umardev500/spk/domain/model"
 )
@@ -38,8 +39,27 @@ func (u *userRepository) Create(ctx context.Context, data model.User) (err error
 	return
 }
 
-func (u *userRepository) Delete(context.Context, model.UserParams) (err error) {
-	panic("unimplemented")
+func (u *userRepository) Delete(ctx context.Context, params model.UserParams) (err error) {
+	log.Debug().Msgf("deleting user")
+	query := `--sql
+		DELETE FROM users WHERE id = ?;
+	`
+
+	db := u.trx.GetConn(ctx)
+	result, err := db.ExecContext(ctx, query, params.ID)
+	if err != nil {
+		log.Error().Msgf("error deleting user: %v", err)
+		return
+	}
+
+	affected, _ := result.RowsAffected()
+	if affected == 0 {
+		log.Error().Msgf("user with id %s not found", params.ID)
+		return constants.ErrorNotAffected
+	}
+
+	log.Debug().Msgf("deleted user with id: %s", params.ID)
+	return
 }
 
 func (u *userRepository) Find(context.Context, model.UserFind) (users []model.User, err error) {
