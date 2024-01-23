d74a29911b8a1c2694811bff88af9c7abfd7fbfc
Umar <umar.dev500@gmail.com>
2024-01-23 07:13:00 +0700

    scaffold basic CRUD in repository

--- application/repository/user.go
+++ application/repository/user.go
@@ -24,7 +24,7 @@ func (u *userRepository) Create(ctx context.Context, data model.User) (err error
 	log.Debug().Msgf("creating user: %v", data)
 	query := `--sql
 		INSERT INTO users (id, email, password, status)
-		VALUES (?, ?, ?, ?);
+		VALUES ($1, $2, $2, $4);
 	`
 
 	db := u.trx.GetConn(ctx)
@@ -42,7 +42,7 @@ func (u *userRepository) Create(ctx context.Context, data model.User) (err error
 func (u *userRepository) Delete(ctx context.Context, params model.UserParams) (err error) {
 	log.Debug().Msgf("deleting user")
 	query := `--sql
-		DELETE FROM users WHERE id = ?;
+		DELETE FROM users WHERE id = $1;
 	`
 
 	db := u.trx.GetConn(ctx)
@@ -62,14 +62,73 @@ func (u *userRepository) Delete(ctx context.Context, params model.UserParams) (e
 	return
 }
 
-func (u *userRepository) Find(context.Context, model.UserFind) (users []model.User, err error) {
-	panic("unimplemented")
+func (u *userRepository) Find(ctx context.Context, find model.UserFind) (users []model.User, err error) {
+	query := `--sql
+		SELECT * FROM users
+	`
+
+	db := u.trx.GetConn(ctx)
+	cur, err := db.QueryxContext(ctx, query)
+	if err != nil {
+		log.Error().Msgf("")
+		return
+	}
+
+	users = make([]model.User, 0)
+
+	for cur.Next() {
+		var each model.User
+		if err := cur.StructScan(&each); err != nil {
+			return nil, err
+		}
+	}
+
+	return
 }
 
-func (u *userRepository) FindOne(context.Context, model.UserFind) (user model.User, err error) {
-	panic("unimplemented")
+func (u *userRepository) FindOne(ctx context.Context, find model.UserFind) (user model.User, err error) {
+	query := `--sql
+		SELECT * FROM users
+	`
+
+	db := u.trx.GetConn(ctx)
+
+	err = db.QueryRowxContext(ctx, query).StructScan(&user)
+	if err != nil {
+		return
+	}
+
+	return
 }
 
-func (u *userRepository) Update(context.Context, model.UserUpdate) (err error) {
-	panic("unimplemented")
+func (u *userRepository) Update(ctx context.Context, data model.UserUpdate) (err error) {
+	query := `--sql
+		UPDATE users SET email = $2, password = $3, status = $4 WHERE id = $1
+	`
+	id := data.Params.ID
+	userData := data.Data
+
+	db := u.trx.GetConn(ctx)
+	result, err := db.ExecContext(
+		ctx,
+		query,
+		id,
+		userData.Email,
+		userData.Password,
+		userData.Status,
+	)
+	if err != nil {
+		return
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil {
+		return
+	}
+
+	if affected == 0 {
+		return constants.ErrorNotAffected
+	}
+
+	return
 }
