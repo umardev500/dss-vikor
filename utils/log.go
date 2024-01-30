3792ea9e28dcbd23142e01cfc6ac89c2c0a19f28
Umar <umar.dev500@gmail.com>
2024-01-30 11:28:24 +0700

    add case no data in log util

--- utils/log.go
+++ utils/log.go
@@ -13,6 +13,11 @@ func LogBuilder(uuid uuid.UUID, msg string, data interface{}, err error) string 
 		msgStr = color.RedString(msg)
 	}
 
+	if data == nil {
+		logStr := fmt.Sprintf("uuid: %s | msg: %s", color.GreenString(uuid.String()), msgStr)
+		return logStr
+	}
+
 	logStr := fmt.Sprintf("uuid: %s | msg: %s | data: %v", color.GreenString(uuid.String()), msgStr, data)
 	return logStr
 }
