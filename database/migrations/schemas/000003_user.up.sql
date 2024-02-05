65860af737461ee8b294d7608d3bfa8458ba04e1
Umar <umar.dev500@gmail.com>
2024-02-05 17:14:50 +0700

    add trigger to user

--- database/migrations/schemas/000003_user.up.sql
+++ database/migrations/schemas/000003_user.up.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS users (
     deleted_at timestamptz DEFAULT NULL,
     version int DEFAULT 0
 );
+
+-- create index of status
+CREATE INDEX idx_user_status ON users(status);
+
+-- Create trigger
+CREATE TRIGGER user_update_trigger
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+        WHEN (
+            NEW.email IS DISTINCT FROM OLD.email OR
+            NEW.password IS DISTINCT FROM OLD.password OR
+            NEW.status IS DISTINCT FROM OLD.status
+        )
+EXECUTE FUNCTION update_function();
