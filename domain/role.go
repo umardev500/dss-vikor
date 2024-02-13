a25025718d1500df6e0d3cc8c26e225bcac02f3a
Umar <umar.dev500@gmail.com>
2024-02-13 22:30:27 +0700

    remove unused interface

--- domain/role.go
+++ domain/role.go
@@ -3,14 +3,9 @@ package domain
 import (
 	"context"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/umardev500/spk/domain/model"
 )
-
-type RoleDelivery interface {
-	Create(c *fiber.Ctx) error
-}
 
 type RoleUsecase interface {
 	Create(ctx context.Context, role model.RoleCreate) model.Response
